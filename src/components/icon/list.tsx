0b3d5ef70c6719e1f7982f88435a14143a6af8af
Harsh Zalavadiya <harshzalavadiya@users.noreply.github.com>
2020-10-19 19:34:12 +0530

    :bug: Clipboard encoded URL (#7)

--- src/components/icon/list.tsx
+++ src/components/icon/list.tsx
@@ -67,7 +67,7 @@ const iconList: IconListType = {
       <path d="M16 1H4a2 2 0 00-2 2v14h2V3h12V1zm3 4H8a2 2 0 00-2 2v14c0 1.1.9 2 2 2h11a2 2 0 002-2V7a2 2 0 00-2-2zm0 16H8V7h11v14z" />
     ),
     color: "#718096",
-    e: l => navigator.clipboard.writeText(l),
+    e: l => navigator.clipboard.writeText(decodeURIComponent(l)),
   },
 };
 
